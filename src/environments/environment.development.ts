73d777be9250f4e530f5dec619008a21bdbbb1a1
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-04-09 18:06:23 +0530

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:true,
-    baseApiUrl:"http://localhost:7098/api",
+    baseApiUrl:"https://localhost:7098/api",
     baseApiUrl1:"https://localhost:7078/api",
     baseApiUrlavail:"https://localhost:7068/api",
     baseApiUrlallergy:"https://localhost:7163/Whizcare",
