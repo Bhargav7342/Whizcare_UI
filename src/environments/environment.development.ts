1904e6c1966ad1f8312afcd5d2ab78f23af85397
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-04-09 23:16:21 +0530

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:true,
-    baseApiUrl:"https://patservices.azurewebsites.net/api",
+    baseApiUrl:"https://localhost:7078/api",
     baseApiUrl1:"https://localhost:7078/api",
     baseApiUrlavail:"https://localhost:7068/api",
     baseApiUrlallergy:"https://localhost:7163/Whizcare",
