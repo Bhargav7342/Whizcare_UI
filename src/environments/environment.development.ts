0cd27acbf02a037f26fb577d527ac1f70c34ddd1
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-04-10 10:46:45 +0530

    Updated frontend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production:true,
     baseApiUrl:"https://patservices.azurewebsites.net/api",
-    baseApiUrl1:"https://localhost:7078/api",
-    baseApiUrlavail:"https://localhost:7068/api",
-    baseApiUrlallergy:"https://localhost:7163/Whizcare",
-    baseApiUrlAudit:"https://localhost:7157/api",
+    baseApiUrl1:"https://appointmentserv.azurewebsites.net/api",
+    baseApiUrlavail:"https://doctorserv.azurewebsites.net/api",
+    baseApiUrlallergy:"https://allergy.azurewebsites.net/api",
+    baseApiUrlAudit:"https://auditserve.azurewebsites.net/api",
 };
