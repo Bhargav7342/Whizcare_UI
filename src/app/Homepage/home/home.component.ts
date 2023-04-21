a0cf5bb3c2fa74971d46dff8105b8b3c077692d8
Sai Krishna <saikrishnahsr0@gmail.com>
2023-04-21 14:15:46 +0530

    Minor Frontend changes

--- src/app/Homepage/home/home.component.ts
+++ src/app/Homepage/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,5 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
+  constructor(private router:Router){}
+  PatientLogin(){
+    // this.status="logout";
+    this.router.navigate(['/plogin']);
+  }
 }
